59091a41131796f6eed49acb7d06aad3b040ec8a
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-11-17 02:28:22 +0500

    Переменные и метод onCreate. Дегтянников.

--- app/src/main/java/com/example/simplesqlitess/MainActivity.java
+++ app/src/main/java/com/example/simplesqlitess/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.simplesqlitess;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText EtName, EtMail;
+    TextView tvContent;
+    DbHelper DbHelper;
+    SQLiteDatabase DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        EtName = findViewById(R.id.etName);
+        EtMail = findViewById(R.id.etMail);
+        tvContent = findViewById(R.id.tvContent);
+        
+        DbHelper = new DbHelper(this);
+        DB = DbHelper.getWritableDatabase();
     }
 }
