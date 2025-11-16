ad37ff39e4caa13277a259463f8316751df62c58
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-11-17 02:35:39 +0500

    Разработанные методы onAdd, onRead и onDelete. Дегтянников.

--- app/src/main/java/com/example/simplesqlitess/MainActivity.java
+++ app/src/main/java/com/example/simplesqlitess/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.simplesqlitess;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -28,5 +31,30 @@ public class MainActivity extends AppCompatActivity {
         
         DbHelper = new DbHelper(this);
         DB = DbHelper.getWritableDatabase();
+    }
+    public void onAdd(View view)
+    {
+        ContentValues CV = new ContentValues();
+        CV.put("Name",EtName.getText().toString());
+        CV.put("Mail",EtMail.getText().toString());
+        DB.insert("Users",null, CV);
+    }
+    public void onRead(View view)
+    {
+        String Data = "";
+
+        Cursor C = DB.query("Users",null,null,null,null,null, null);
+        if(C.moveToFirst())
+        {
+            do {
+                Data += "Id=" + C.getInt(0) + ", Name=" + C.getString(1) + ", Mail=" + C.getString(2) + "\r\n"
+            } while (C.moveToNext());
+        }
+        C.close();
+        tvContent.setText(Data);
+    }
+    public  void  OnDelete(View view)
+    {
+        DB.delete("Users",null,null);
     }
 }
